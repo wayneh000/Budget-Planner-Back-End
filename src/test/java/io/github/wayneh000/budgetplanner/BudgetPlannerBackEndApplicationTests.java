9d6e3b9e6d8361259bc1c3c425a35d2ee32b46d5
Wayne He <70216125+wayneh000@users.noreply.github.com>
2022-06-27 22:01:44 -0500

    Initial commit.

---
+++ src/test/java/io/github/wayneh000/budgetplanner/BudgetPlannerBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wayneh000.budgetplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetPlannerBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
