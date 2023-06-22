327122bd4d3b169b5eded7332b151badfe941461
Wayne He <70216125+wayneh000@users.noreply.github.com>
2023-06-22 04:41:28 -0400

    Added BudgetPlannerException

---
+++ src/main/java/io/github/wayneh000/budgetplanner/exception/BudgetPlannerException.java
@@ -0,0 +1,10 @@
+package io.github.wayneh000.budgetplanner.exception;
+
+public class BudgetPlannerException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7671335385040237994L;
+
+}
