14f7a15a18190bc79726803791d46f90b58ead8f
Wayne He <70216125+wayneh000@users.noreply.github.com>
2023-07-27 04:42:59 -0400

    Fixed request mapping bug.

--- src/main/java/io/github/wayneh000/budgetplanner/controller/AccountController.java
+++ src/main/java/io/github/wayneh000/budgetplanner/controller/AccountController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,8 @@ import io.github.wayneh000.budgetplanner.request.UpdatePasswordRequest;
 import io.github.wayneh000.budgetplanner.response.AccountResponse;
 import io.github.wayneh000.budgetplanner.service.AccountService;
 
-@RestController("/api/v1/auth")
+@RestController
+@RequestMapping("/api/v1/auth")
 public class AccountController {
 
 	@Autowired
