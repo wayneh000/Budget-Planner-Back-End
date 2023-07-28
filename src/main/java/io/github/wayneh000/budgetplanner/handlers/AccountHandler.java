254a0c9506558e370c7e01883c828f8884356e60
Wayne He <70216125+wayneh000@users.noreply.github.com>
2023-07-28 02:34:13 -0400

    Added after advices for createAccount()

--- src/main/java/io/github/wayneh000/budgetplanner/handlers/AccountHandler.java
+++ src/main/java/io/github/wayneh000/budgetplanner/handlers/AccountHandler.java
@@ -3,6 +3,8 @@ package io.github.wayneh000.budgetplanner.handlers;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -12,13 +14,25 @@ import io.github.wayneh000.budgetplanner.request.AccountRequest;
 @Aspect
 @Component
 public class AccountHandler {
-	
+
 	private static final Logger LOGGER = LogManager.getLogger();
 
 	@Before("execution(* io.github.wayneh000.budgetplanner.service.AccountService.createAccount(..))")
 	public void createAccountBeforeAdvice(JoinPoint joinPoint) {
-		LOGGER.info("Creating account with details: ");
+		LOGGER.info("Creating account with details:");
 		AccountRequest request = (AccountRequest) joinPoint.getArgs()[0];
 		LOGGER.info(request);
+	}
+
+	@AfterReturning(pointcut = "execution(* io.github.wayneh000.budgetplanner.service.AccountService.createAccount(..))", returning = "account")
+	public void createAccountAfterReturning(Object account) {
+		LOGGER.info("Account has been created with details:");
+		LOGGER.info(account);
+	}
+
+	@AfterThrowing(pointcut = "execution(* io.github.wayneh000.budgetplanner.service.AccountService.createAccount(..))", throwing = "e")
+	public void createAccountAfterThrowing(Exception e) {
+		LOGGER.error("Account creation failed for the following reason: ");
+		LOGGER.error(e.getMessage());
 	}
 }
