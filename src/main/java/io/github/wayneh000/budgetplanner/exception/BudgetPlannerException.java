2cb134304decf3ceddeba139314e388be5bafa3a
Wayne He <70216125+wayneh000@users.noreply.github.com>
2023-07-27 22:53:25 -0400

    Removed ResponseStatus

--- src/main/java/io/github/wayneh000/budgetplanner/exception/BudgetPlannerException.java
+++ src/main/java/io/github/wayneh000/budgetplanner/exception/BudgetPlannerException.java
@@ -1,9 +1,5 @@
 package io.github.wayneh000.budgetplanner.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class BudgetPlannerException extends Exception {
 
 	/**
