c6b9ecce73be8e2c67e34bed5a8badd5a37feb27
Wayne He <70216125+wayneh000@users.noreply.github.com>
2023-07-27 22:42:45 -0400

    Remove unused imports.

--- src/main/java/io/github/wayneh000/budgetplanner/service/AccountService.java
+++ src/main/java/io/github/wayneh000/budgetplanner/service/AccountService.java
@@ -4,8 +4,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
