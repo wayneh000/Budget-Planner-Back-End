918a1707542c22daa8e7fbf4abfde9d04b5c31bf
Wayne He <70216125+wayneh000@users.noreply.github.com>
2023-07-28 04:35:33 -0400

    Fixed bug on endpoint not taking payload.

--- src/main/java/io/github/wayneh000/budgetplanner/controller/SessionController.java
+++ src/main/java/io/github/wayneh000/budgetplanner/controller/SessionController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -23,7 +24,7 @@ public class SessionController {
 	private SessionService sessionService;
 	
 	@PostMapping("login")
-	public ResponseEntity<SessionResponse> login(AccountRequest request) {
+	public ResponseEntity<SessionResponse> login(@RequestBody AccountRequest request) {
 		try {
 			return new ResponseEntity<>(createResponse(sessionService.createSession(request)), HttpStatus.CREATED);
 		} catch (BudgetPlannerException e) {
