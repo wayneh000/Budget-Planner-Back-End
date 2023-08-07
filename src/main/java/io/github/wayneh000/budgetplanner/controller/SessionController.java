18de9031d30ee1c33916060859fce3e35e97788c
Wayne He <70216125+wayneh000@users.noreply.github.com>
2023-08-07 07:42:11 -0400

    Implemented verify session endpoint.

--- src/main/java/io/github/wayneh000/budgetplanner/controller/SessionController.java
+++ src/main/java/io/github/wayneh000/budgetplanner/controller/SessionController.java
@@ -3,6 +3,8 @@ package io.github.wayneh000.budgetplanner.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +34,24 @@ public class SessionController {
 					e.getMessage().equals(ErrorMessages.ACCOUNT_INVALID_CREDENTIALS) ? HttpStatus.UNAUTHORIZED
 							: HttpStatus.INTERNAL_SERVER_ERROR,
 					e.getMessage());
+		}
+	}
+	
+	@GetMapping("verify/{id}")
+	public ResponseEntity<SessionResponse> verify(@PathVariable String id) {
+		try {
+			return new ResponseEntity<>(createResponse(sessionService.verifySession(id)), HttpStatus.OK);
+		} catch (BudgetPlannerException e) {
+			HttpStatus status;
+			switch(e.getMessage()) {
+				case ErrorMessages.SESSION_EXPIRED:
+				case ErrorMessages.SESSION_NOT_FOUND:
+					status = HttpStatus.UNAUTHORIZED;
+					break;
+				default:
+					status = HttpStatus.INTERNAL_SERVER_ERROR;
+			}
+			throw new ResponseStatusException(status, e.getMessage());
 		}
 	}
 	
