69db04c5a357a8de365153f29240e57671fd197f
Wayne He <70216125+wayneh000@users.noreply.github.com>
2023-08-07 22:06:05 -0400

    Period now holds name and value.

--- src/main/java/io/github/wayneh000/budgetplanner/util/Period.java
+++ src/main/java/io/github/wayneh000/budgetplanner/util/Period.java
@@ -2,16 +2,21 @@ package io.github.wayneh000.budgetplanner.util;
 
 public class Period {
 
-	public static final int HOURLY = 0; //special case
-	public static final int DAILY = 1;
-	public static final int WEEKLY = 7;
-	public static final int BIWEEKLY = 14;
-	public static final int SEMIMONTHLY = 15;
-	public static final int MONTHLY = 30;
-	public static final int QUARTERLY = 90;
-	public static final int SEMIANNUAL = 180;
-	public static final int ANNUAL = 360;
+	public static final Period HOURLY = new Period("Hourly", 0); //special case
+	public static final Period DAILY = new Period("Daily", 1);
+	public static final Period WEEKLY = new Period("Weekly", 7);
+	public static final Period BIWEEKLY = new Period("Biweekly", 14);
+	public static final Period SEMIMONTHLY = new Period("Semimonthly", 15);
+	public static final Period MONTHLY = new Period("Monthly", 30);
+	public static final Period QUARTERLY = new Period("Quarterly", 90);
+	public static final Period SEMIANNUAL = new Period("Semiannual", 180);
+	public static final Period ANNUAL = new Period("Annual", 360);
 	
-	private Period() {
+	public final String name;
+	public final int value;
+	
+	private Period(String name, int value) {
+		this.name = name;
+		this.value = value;
 	}
 }
